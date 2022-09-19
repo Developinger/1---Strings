24a5fa468c169a808b3590f33e57027002534fb5
Homaine Blair <homaineblair@gmail.com>
2022-09-19 12:21:31 -0400

    v1

--- Lab - Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
+++ Lab - Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
@@ -3,6 +3,16 @@
  
  Create two constants, `nameInCaps` and `name`. Assign `nameInCaps` your name as a string literal with proper capitalization. Assign `name` your name as a string literal in all lowercase. Write an if-else statement that checks to see if `nameInCaps` and `name` are the same. If they are, print "The two strings are equal," otherwise print "The two strings are not equal."
  */
+let nameInCaps = "Homaine"
+let name = "homaine"
+
+if nameInCaps == name {
+    print("The two strings are equal.")
+} else {
+        print("The two strings are not equal.")
+}
+
+
 
 
 /*:
@@ -14,10 +24,18 @@
  
 - "<INSERT LOWERCASED VERSION OF `nameInCaps` HERE> and <INSERT LOWERCASED VERSION OF `name` HERE> are not the same."
  */
-
+if nameInCaps.lowercased() == name {
+    print("\(nameInCaps) and \(name) are the same.")
+}else{
+    print("\(nameInCaps) and \(name) are not the same.")
+}
 
 //:  Imagine you are looking through a list of names to find any that end in "Jr." Write an if statement below that will check if `junior` has the suffix "Jr." If it does, print "We found a second generation name!"
 let junior = "Cal Ripken Jr."
+
+if junior.contains("Jr."){
+    print("We found a second generation name!")
+}
 
 
 
@@ -26,9 +44,14 @@ import Foundation
 let textToSearchThrough = "To be, or not to be--that is the question"
 let textToSearchFor = "to be, or not to be"
 
+if textToSearchThrough.lowercased().contains(textToSearchFor){
+    print("I found it!")
+}else{
+    print("Not found")
+}
 
 //:  Print to the console the number of characters in your name by using the `count` property on `name`.
-
+print(name.count)
 
 /*:
 [Previous](@previous)  |  page 4 of 5  |  [Next: App Exercise - Password Entry and User Search](@next)
